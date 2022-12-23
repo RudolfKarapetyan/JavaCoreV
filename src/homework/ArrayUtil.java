4421f725cd14c10fcf9afa9034fd50d3255c24c5
rudolf <rudolfkarapetyan222111@gmail.com>
2022-12-23 03:27:50 -0800

    Home-work 3

---
+++ src/homework/ArrayUtil.java
@@ -0,0 +1,33 @@
+package homework;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {2,43,54,25,74,3,56,7,4,1};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("\n");
+        //
+        //
+        //
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]){
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+        System.out.println("\n");
+        //
+        //
+        //
+        int min = max;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]){
+                min = array[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
--- src/homework/homework1/IfForExample.java
+++ src/homework/homework1/IfForExample.java
@@ -2,7 +2,7 @@ package homework.homework1;
 
 public class IfForExample {
     public static void main(String[] args) {
-        int a = 23, b = 44,x,y;
+        int a = 23, b = 44, x, y;
         if (a > b) {
             System.out.println("a > b");
             x = a;
@@ -11,7 +11,7 @@ public class IfForExample {
             System.out.println("b > a");
             x = b;
             y = a;
-        }else {
+        } else {
             System.out.println("a = b");
             x = a;
             y = b;
@@ -27,15 +27,15 @@ public class IfForExample {
 //
 //
 //
-        System.out.println((char)a);
-        System.out.println((char)b);
+        System.out.println((char) a);
+        System.out.println((char) b);
         System.out.println("\n");
 //
 //
 //
-        if(a==b){
+        if (a == b) {
             System.out.println("a-ն հավասար է b-ին");
-        }else{
+        } else {
             System.out.println("a-հավասար չէ b-ին");
         }
     }
