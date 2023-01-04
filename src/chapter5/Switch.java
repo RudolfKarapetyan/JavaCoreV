543a68ee8d17926aaddacb4d13e82023dd736b56
rudolf <rudolfkarapetyan222111@gmail.com>
2023-01-04 05:23:53 -0800

    Chapter 5

---
+++ src/chapter5/Switch.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+public class Switch {
+    public static void main(String[] args) {
+        int month = 4;
+        String season;
+        switch (month){
+            case 12:
+            case 1:
+            case 2:
+                season = "зиме";
+        }
+    }
+}
