8b832cdca06ac999dfdd19fc51f8ec5f37b29537
rudolf <rudolfkarapetyan222111@gmail.com>
2023-01-07 10:09:40 -0800

    Home-work 5 error correction.

--- src/homework/homework5/ArraySpaceExample.java
+++ src/homework/homework5/ArraySpaceExample.java
@@ -3,24 +3,30 @@ package homework.homework5;
 public class ArraySpaceExample {
     public static void main(String[] args) {
         char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
-        char[] result = new char[spaceArray.length];
         int removerStart = 0, removerEnd = spaceArray.length;
+        int resultLenght = spaceArray.length;
         for (int i = 0; i < spaceArray.length; i++) {
             if (spaceArray[i] == ' ') {
                 removerStart++;
+                resultLenght--;
             } else {
                 break;
             }
             for (int j = spaceArray.length - 1; j > 0; j--) {
                 if (spaceArray[j] == ' ') {
                     removerEnd--;
+                    resultLenght--;
                 } else {
                     break;
                 }
             }
         }
-        for (int i = removerStart; removerStart < removerEnd; removerStart++, i++) {
-            result[i] = spaceArray[removerStart];
+        char[] result = new char[resultLenght];
+        for (int i = removerStart, j = 0; removerStart < removerEnd; removerStart++, i++, j++) {
+            result[j] = spaceArray[removerStart];
+
+        }
+        for (int i = 0; i < result.length; i++) {
             System.out.print(result[i]);
         }
     }
